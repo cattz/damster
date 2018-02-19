efe2bace1e75ba45ec20a0b7c02fd2dbc82d3561
Xabier Davila <davila.xabier@gmail.com>
2018-02-19 14:15:13 +0100

    Loop for monthly report

--- damster/builds_report.py
+++ damster/builds_report.py
@@ -7,15 +7,18 @@ log = initialize_logger(__name__)
 
 cfg = get_config()
 
+YEAR = '2017'
+
 
 def main():
 
-    report = BambooBuildsReport(
-        cfg,
-        from_date='2018-01-01',
-        to_date='2018-02-28'
-    )
-    report.run_report(use_cache=True)
+    for month in range(1, 11):
+        report = BambooBuildsReport(
+            cfg,
+            from_date='{}-{:02}-01'.format(YEAR, month),
+            to_date='{}-{:02}-01'.format(YEAR, month+1)
+        )
+        report.run_report(use_cache=True)
 
 
 if __name__ == "__main__":
