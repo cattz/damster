4daee45a33104ff703dc8a706c025772bed65a5f
Xabier Davila <xabier.davilafreire@aegon.nl>
2018-05-25 11:39:16 +0200

    Quick fix for confluence reports not exiting properly

--- damster/reports/confluence/commands.py
+++ damster/reports/confluence/commands.py
@@ -14,4 +14,8 @@ def confluence_changes(ctx, from_date, to_date, use_ssh_tunnel):
     confluence_report = ConfluenceChanges(ctx.obj, from_date, to_date, use_ssh_tunnel=use_ssh_tunnel)
     confluence_report.save_to_csv()
     confluence_report.save_to_json()
-    # confluence_report.save_to_html(title='Confluence Reports: Changes')
+
+    # For some reason, this class destructor doesn't always run, so adding this here to make sure
+    # The scripts actually ends
+    if confluence_report.ssh_tunnel:
+        confluence_report.ssh_tunnel.close()
